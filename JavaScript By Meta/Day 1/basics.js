6c16c116a42a8a47f9d5e3ce6e7103de826a2109
VihaanDev <96285502+vihaanishere@users.noreply.github.com>
2026-06-05 18:01:18 +0530

    Add JS fundamentals: variables, data types, conditionals, and basics
    Covered core JavaScript concepts including variable declarations, primitive data types, conditional statements, and basic syntax as part of the Meta Front-End Developer Certificate - Course 2.

---
+++ JavaScript By Meta/Day 1/basics.js
@@ -0,0 +1,18 @@
+// Single Line Comment 
+
+/*
+This
+Is
+Multi
+Line
+Comment
+*/
+
+//Print Outputs
+console.log("Hello World");
+
+//Styling Console Outputs
+console.log("%cHello, World", "color: blue; font-size: 40px");
+
+//Console Log Multiple Values
+console.log("Hello", "there,", "World");
