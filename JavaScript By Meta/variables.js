601a5566e2da28680ba5324e0b438b311c8c6c5c
VihaanDev <96285502+vihaanishere@users.noreply.github.com>
2026-06-05 17:52:41 +0530

    Add JS fundamentals: variables, data types, conditionals, and basics
    Covered core JavaScript concepts including variable declarations, primitive data types, conditional statements, and basic syntax as part of the Meta Front-End Developer Certificate - Course 2.

---
+++ JavaScript By Meta/variables.js
@@ -0,0 +1,8 @@
+//Assigning Variables
+//Syntax: var <variable-name> = <variable-value>;
+var name = "Vihaan";
+//Using variable in console log
+console.log("Hello,", name);
+//Changing variable value
+name = "World";
+console.log("Hello,", name);
