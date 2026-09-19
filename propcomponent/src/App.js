2083f234f1949b039db67954c52f30047bd02b12
VihaanDev <96285502+vihaanishere@users.noreply.github.com>
2026-09-19 10:31:32 +0530

    Meta Front End Course Progress

---
+++ propcomponent/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header"
+
+
+function App() {
+  return (
+      <div>
+        <Header name = "Anna" color = "puple" />
+        
+      </div>
+  );
+}
+
+export default App;
---
+++ propcomponent/src/components/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Header(props){
+    console.log(props)
+    return <h1>Hello there indeed</h1>
+}
+export default Header;
